d466703e6a31a1ac40672d0d7c151bd88325086f
Stanislav Hadzhipopov <s.hadzhipopov@hotmail.com>
2025-06-06 14:06:02 +0200

    saved price configuration to db

--- AppServices/IPriceCalculatorService.cs
+++ AppServices/IPriceCalculatorService.cs
@@ -1,6 +1,9 @@
-﻿namespace AppServices
+﻿using EndreinigungZurich.Models.Booking;
+
+namespace AppServices
 {
     public interface IPriceCalculatorService
     {
+        Task<decimal> CalculateTotalAsync(PropertyConfigurationViewModel config, int priceSetId);
     }
 }
