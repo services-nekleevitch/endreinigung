b46f19c63bb48f7a1feb312db420e2ea688c39b0
Stanislav Hadzhipopov <s.hadzhipopov@hotmail.com>
2025-10-31 09:39:00 +0100

    removed unnecessary attributes

--- DbAccess/Entities/Location.cs
+++ DbAccess/Entities/Location.cs
@@ -9,17 +9,8 @@ namespace DbAccess.Entities
     public class LocationCH
     {
         public int Id { get; set; }
-
-        public string ZipCode { get; set; } = null!;
-
         public string PlaceName { get; set; } = null!;
-
-        public string MunicipalityName { get; set; }
-
-        public int? MunicipalityNo { get; set; }
-
+        public string ZipCode { get; set; } = null!;
         public string Canton { get; set; }
-
-        public string ExtraDigit { get; set; }
     }
 }
